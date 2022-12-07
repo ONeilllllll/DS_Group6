69a8e70a90ae04765f6b52b7905dd8586264eb38
Ray Zheng <ray334723918@gmail.com>
2022-12-07 21:40:27 +0800

    keyword

---
+++ beat_google/src/beat_google/Keyword.java
@@ -0,0 +1,15 @@
+package beat_google;
+
+public class Keyword {
+	String name;
+	double weight;
+
+	public Keyword(String name, double weight) {
+		this.name = name;
+		this.weight = weight;
+	}
+
+	public String toString() {
+		return "";
+	}
+}
