ab4b6e8c5536f7e4278c2fb2602c511e194b0f64
ericwang <ericwang@140.119.159.12>
2022-12-07 22:09:34 +0800

    Merge branch 'master' of https://github.com/ONeilllllll/DS_Group6.git

--- beat_google/src/beat_google/Page.java
+++ beat_google/src/beat_google/Page.java
@@ -2,4 +2,16 @@ package beat_google;
 
 public class Page {
 
+	protected String url;
+	protected String name;
+	protected WordCounter counter;
+	protected Double score;
+	
+	public Page() {
+		
+	}
+	
+	public void setScore() {
+		
+	}
 }
--- beat_google/src/beat_google/WordCounter.java
+++ beat_google/src/beat_google/WordCounter.java
@@ -2,4 +2,13 @@ package beat_google;
 
 public class WordCounter {
 
+protected String content;
+	
+	public WordCounter() {
+		
+	}
+	
+	public int countKeyword() {
+		return 0;
+	}
 }
